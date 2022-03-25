Fixed wrong rotation name in code

omniwheel_ros/src/omniwheel/omniwheel/path_visualizer.py
@@ -63,7 +63,7 @@ class PathVisualizer(Node):
         self.display_offset = np.array([self.WIDTH / 2.2, self.HEIGHT / 2])
 
     def pose_callback(self, msg):
-        self.orientation = msg.orientation.z
+        self.orientation = msg.orientation.rot
         self.position = (msg.position.x, msg.position.y)
         self.waypoints.append(self.position)
 
@@ -99,7 +99,7 @@ class PathVisualizer(Node):
 
     def resetPosition(self):
         request = SetPose.Request()
-        request.position.x, request.position.y, request.orientation.z = 0.0, 0.0, 0.0
+        request.pose.x, request.pose.y, request.pose.rot = 0.0, 0.0, 0.0
         self.set_position_future = self.position_client.call_async(request)
 
     def tick(self):
@@ -219,7 +219,7 @@ class PathVisualizer(Node):
         try:
             response = self.set_position_future.result()
             self.position = (response.pose.x, response.pose.y)
-            self.orientation = response.pose.z
+            self.orientation = response.pose.rot
             self.waypoints = [self.position]
         except Exception as e:
             self.get_logger().info(

omniwheel_ros/src/omniwheel/omniwheel/teensy_node.py
@@ -92,8 +92,8 @@ class TeensyNode(Node):
 
     def set_position_callback(self, request, response):
         self.position = np.array((request.pose.x, request.pose.y))
-        self.orientation = request.pose.z
-        response.pose.x, response.pose.y, response.pose.z = request.pose.x, request.pose.y, request.pose.z
+        self.orientation = request.pose.rot
+        response.pose.x, response.pose.y, response.pose.rot = request.pose.x, request.pose.y, request.pose.rot
         return response
         
     def getTeensyVelocity(self):
@@ -138,7 +138,7 @@ class TeensyNode(Node):
             dy = dist * np.sin(alpha + self.orientation + np.pi / 2)
             self.position += np.array([dx, dy])
             message = Pose()
-            message.position.x, message.position.y, message.orientation.z = self.position[0], self.position[1], self.orientation
+            message.position.x, message.position.y, message.orientation.rot = self.position[0], self.position[1], self.orientation
             self.odometry.publish(message)
 
 