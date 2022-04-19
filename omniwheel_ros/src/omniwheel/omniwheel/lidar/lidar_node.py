a32950f8cb8d912c63d6e731063a93b6a44831b5
Baldhat <baldhat@users.noreply.github.com>
2022-04-19 13:06:45 +0200

    trying to introduce hard offset

--- omniwheel_ros/src/omniwheel/omniwheel/lidar/lidar_node.py
+++ omniwheel_ros/src/omniwheel/omniwheel/lidar/lidar_node.py
@@ -5,6 +5,7 @@ import cv2
 
 import rclpy
 from rclpy.node import Node
+from rclpy.time import Time
 
 from sensor_msgs.msg import PointCloud2, PointField
 
@@ -48,7 +49,9 @@ class LidarNode(Node):
 
     def publish_points(self, points):
         msg = PointCloud2()
-        msg.header.stamp = self.get_clock().now().to_msg()
+        time = self.get_clock().now()
+        time = Time(time.nanoseconds + 40 * 1000000000, time.clock_type)
+        msg.header.stamp = time.to_msg()
         msg.header.frame_id = 'lidar_link'
         msg.width = 320
         msg.height = 240
