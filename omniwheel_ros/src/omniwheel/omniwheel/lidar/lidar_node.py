9d8841bb4882fcda467bd2312929f3225c0ab10c
Baldhat <baldhat@users.noreply.github.com>
2022-04-19 13:10:04 +0200

    Reducing time

--- omniwheel_ros/src/omniwheel/omniwheel/lidar/lidar_node.py
+++ omniwheel_ros/src/omniwheel/omniwheel/lidar/lidar_node.py
@@ -43,9 +43,10 @@ class LidarNode(Node):
 
         points = self.pc.calculate(depth_frame)
         points = np.asarray(points.get_vertices(2), dtype='float32').reshape((320, 240, 3))
-        depth = np.asanyarray(depth_frame.get_data()).reshape((320, 240))
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth, alpha=0.015), cv2.COLORMAP_DEEPGREEN)
-        return np.concatenate((points, depth_colormap), 2)
+        # depth = np.asanyarray(depth_frame.get_data()).reshape((320, 240))
+        # depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth, alpha=0.015), cv2.COLORMAP_DEEPGREEN)
+        # return np.concatenate((points, depth_colormap), 2)
+        return points
 
     def publish_points(self, points):
         msg = PointCloud2()
@@ -60,12 +61,12 @@ class LidarNode(Node):
         itemsize = np.dtype(dtype).itemsize
         msg.fields = [PointField(
             name=n, offset=i * itemsize, datatype=ros_dtype, count=1)
-            for i, n in enumerate('xyzrgb')]
+            for i, n in enumerate('xyz')]
         msg.data = points.tobytes()
         msg.is_dense = False
         msg.is_bigendian = False
-        msg.point_step = 6 * itemsize
-        msg.row_step = 6 * itemsize * 320
+        msg.point_step = 3 * itemsize
+        msg.row_step = 3 * itemsize * 320
 
         self.publisher_.publish(msg)
 
@@ -74,7 +75,6 @@ class LidarNode(Node):
             rclpy.spin_once(self, timeout_sec=0.0)
             points = self.read_points()
             self.publish_points(points)
-            rclpy.spin_once(self, timeout_sec=0.05)
 
 
 def main(args=None):
