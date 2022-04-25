5515db15136b107510537871e21c2329ddb0f90b
Baldhat <baldhat@users.noreply.github.com>
2022-04-25 13:20:00 +0200

    Fixed typo

--- omniwheel_ros/src/omniwheel_lidar/src/lidar_node.cpp
+++ omniwheel_ros/src/omniwheel_lidar/src/lidar_node.cpp
@@ -14,8 +14,6 @@
 using namespace std::chrono_literals;
 
 bool enabled = false;
-const int CAPACITY = 5; // allow max latency of 5 frames
-rs2::frame_queue queue(CAPACITY);
 
 void enable_lidar(const std::shared_ptr<std_srvs::srv::SetBool::Request> request,
                   std::shared_ptr<std_srvs::srv::SetBool::Response> response) {
@@ -29,56 +27,53 @@ class LidarNode : public rclcpp::Node
   public:
     rs2::pipeline p;
     rs2::pointcloud pc;
-    rs2::depth_frame frame;
     LidarNode()
     : Node("lidar_node")
     {
-        pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("lidar/points", 10);
-        enable_service =
+      pub_ = create_publisher<sensor_msgs::msg::PointCloud2>("lidar/points", 10);
+      enable_service =
         create_service<std_srvs::srv::SetBool>("enable_lidar", &enable_lidar);
 
-        // supported resolutions L515: 320x240, 640x480, 1024x768
-        rs2::config cfg;
-        cfg.enable_stream(RS2_STREAM_DEPTH, 320, 240, RS2_FORMAT_Z16, 30);
-        //cfg.enable_stream(RS2_STREAM_INFRARED, 320, 240, RS2_FORMAT_Z16, 30);
-        p.start(cfg);
+      // supported resolutions L515: 320x240, 640x480, 1024x768
+      rs2::config cfg;
+      cfg.enable_stream(RS2_STREAM_DEPTH, 320, 240, RS2_FORMAT_Z16, 30);
+      cfg.enable_stream(RS2_STREAM_INFRARED, 320, 240, RS2_FORMAT_Z16, 30);
+      p.start(cfg);
 
-        RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready...");
+      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Ready...");
     }
     void run() {
-      std::thread t([&]() {
-        while (true){
-            if (queue.poll_for_frame(&frame)) {
-                cloud_.points.clear();
-                auto points = pc.calculate(frame.get_data());
-                auto vertices = points.get_vertices();
-                for (size_t i = 0; i < points.size(); i++) {
-                    pcl::PointXYZI pt = pcl::PointXYZI();
-                    if (vertices[i].x != 0 || vertices[i].y != 0 || vertices[i].z != 0) {
-                        pt.x = vertices[i].x;
-                        pt.y = vertices[i].y;
-                        pt.z = vertices[i].z;
-                        pt.intensity = 1.0;
-                        cloud_.points.push_back(pt);
-                    }
-                }
-                pcl::PCLPointCloud2 tmp_cloud;
-                pcl::toPCLPointCloud2(cloud_, tmp_cloud);
-                pcl_conversions::fromPCL(tmp_cloud, pc2_msg_);
-
-                pc2_msg_.header.stamp = now();
-                pc2_msg_.header.frame_id = "lidar_link";
-
-                pub_->publish(pc2_msg_);
-            }
-        }
-      });
-      t.detach();
-
       while (true) {
         if (enabled) {
+          std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
           rs2::frameset frames = p.wait_for_frames();
-          queue.enqueue(frames.get_depth_frame());
+
+          rs2::depth_frame depth = frames.get_depth_frame();
+
+          cloud_.points.clear();
+          auto points = pc.calculate(depth);
+          auto vertices = points.get_vertices();
+          for (size_t i = 0; i < points.size(); i++) {
+            pcl::PointXYZI pt = pcl::PointXYZI();
+            if (vertices[i].x != 0 || vertices[i].y != 0 || vertices[i].z != 0) {
+              pt.x = vertices[i].x;
+              pt.y = vertices[i].y;
+              pt.z = vertices[i].z;
+              pt.intensity = 1.0;
+              cloud_.points.push_back(pt);
+            }
+          }
+          pcl::PCLPointCloud2 tmp_cloud;
+          pcl::toPCLPointCloud2(cloud_, tmp_cloud);
+          pcl_conversions::fromPCL(tmp_cloud, pc2_msg_);
+
+          pc2_msg_.header.stamp = now();
+          pc2_msg_.header.frame_id = "lidar_link";
+
+          pub_->publish(pc2_msg_);
+
+          std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+          std::cout << "FPS = " << 1000000.0 / std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << std::endl;
         }
         rclcpp::spin_some(this->get_node_base_interface());
       }
